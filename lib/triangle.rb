26c8243e3361ff6f37bfcf467e178e0f68bd73f2
KerryAnn Harry <kerryann.harry@yahoo.com>
2020-11-24 13:58:29 -0600

    commit tree 5a8392bad3a2b6068c1da928f6c400eb48dc0532

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Triangle
-  # write code here
-end
+  attr_reader :s1, :s2, :s3
+    def initialize(s1, s2, s3)
+      @s1 = s1
+      @s2 = s2
+      @s3 = s3
+    end
+    def valid?
+      s1 > 0 && s2 > 0 && s3 > 0 && s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1
+
+    end
+
+    def kind
+      if
+      !valid?
+         raise TriangleError
+      elsif
+        @s1 == @s2 && @s2 == @s3
+        :equilateral
+      elsif
+        @s1 != @s2 && @s1 != @s3 && @s2 != @s3
+        :scalene
+      else
+        :isosceles
+      end
+    end
+    class TriangleError < StandardError
+    end
+  end
